0113cbcc6792ceb482881a7dafc924f0976200c8
Gideon Gur <gideon.gur@seatid.com>
2016-01-07 18:29:56 +0200

    rspec and gitignore

--- spec/invoice_number_parser_spec.rb
+++ spec/invoice_number_parser_spec.rb
@@ -1,10 +1,34 @@
 require 'invoice_number_parser'
 
 describe InvoiceNumberParser do
-	context 'when to_s' do
-		it "return '?'" do
-			i_c = IllegalCharacter.new
-			expect(i_c.to_s).to eq "?"
+	before(:each) do
+		File.open("./output_files/output_spec.txt","w"){}
+	end
+
+	context 'when parse' do
+		it "write to output_spec.txt the invoice number 819266248" do
+			allow(File).to receive(:foreach).and_yield(" _     _  _  _  _  _     _ \n")
+																			.and_yield("|_|  ||_| _||_ |_  _||_||_|\n")
+																			.and_yield("|_|  | _||_ |_||_||_   ||_|\n")
+																			.and_yield("                             ")
+			allow(InvoiceNumberParser).to receive(:reseat_output).and_return("output_spec.txt")
+			InvoiceNumberParser.parse("input_spec.txt")
+			expect(File.read("./output_files/output_spec.txt")).to eq "819266248\n"
+		end
+	end
+	context 'when parse' do
+		it "write to output_spec.txt the invoice number 819266248 8192662?8 ILLEGAL" do
+			allow(File).to receive(:foreach).and_yield(" _     _  _  _  _  _     _ \n")
+																			.and_yield("|_|  ||_| _||_ |_  _||_||_|\n")
+																			.and_yield("|_|  | _||_ |_||_||_   ||_|\n")
+																			.and_yield("                             ")
+																			.and_yield(" _     _  _  _  _  _     _ \n")
+																			.and_yield("|_|  ||_| _||_ |_  _||_||_|\n")
+																			.and_yield("|_|  | _||_ |_||_||_    |_|\n")
+																			.and_yield("                             ")
+			allow(InvoiceNumberParser).to receive(:reseat_output).and_return("output_spec.txt")
+			InvoiceNumberParser.parse("input_spec.txt")
+			expect(File.read("./output_files/output_spec.txt")).to eq "819266248\n8192662?8 ILLEGAL\n"
 		end
 	end
 end
